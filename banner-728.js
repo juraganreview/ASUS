289b4fa3036541a7e643325554ed7b556b3308b8
juraganreview <52905355+juraganreview@users.noreply.github.com>
2020-12-14 23:35:58 +0700

    Create banner-728.js

---
+++ banner-728.js
@@ -0,0 +1,9 @@
+var ifrm = document.createElement("iframe");
+ifrm.setAttribute("src", "https://asusid.xyz/banner/vivo-banner-728.php");
+ifrm.style.width = "728px";
+ifrm.style.height = "90px";
+ifrm.style.overflow = "hidden";
+ifrm.scrolling = "none";
+
+var div = document.getElementById("banner-asus-index728");
+div.appendChild(ifrm);
